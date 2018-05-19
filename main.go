35c9299c63703b3e2b05c6e2333f095226c1f691
jumpinjan <jgluck@good.com>
2018-05-19 14:53:44 -0400

    recognizes me

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"gocv.io/x/gocv"
 	"image/color"
 	"image"
+	"github.com/machinebox/sdk-go/facebox"
+	"fmt"
+	"bytes"
+
 
 )
 
@@ -15,6 +19,11 @@ var (
 
  
 func main() {
+	
+	fbox := facebox.New("http://localhost:8080")
+	log.Println("got facebox")
+	
+	
 	webcam, err := gocv.VideoCaptureDevice(0)
 	 if err != nil {
 			 log.Fatalf("error opening web cam: %v", err)
@@ -29,7 +38,7 @@ func main() {
 classifier := gocv.NewCascadeClassifier()
 
 defer classifier.Close()
-
+log.Println("classifer opened")
 if !classifier.Load(faceAlgorithm) {
 		log.Printf("Error reading cascade file: %v\n", faceAlgorithm)
 		return
@@ -37,7 +46,8 @@ if !classifier.Load(faceAlgorithm) {
 
 
 	 // open display window
-	 window := gocv.NewWindow("")
+	 window := gocv.NewWindow("find me")
+	 log.Println("window opened for display")
 	 defer window.Close()
 
 	 for {
@@ -46,17 +56,40 @@ if !classifier.Load(faceAlgorithm) {
 					 continue
 			 }
 			 
-			 
+			 log.Println("read  from webcam")
 			 rects := classifier.DetectMultiScale(img)
+			 log.Println("have rectangles")
 	 for _, r := range rects {
 			 // Save each found face into the file
+			 
 			 imgFace := img.Region(r)
-			 imgFace.Close()
+			 log.Println("got region")
+			 defer imgFace.Close()
+			 
+			 buf, err := gocv.IMEncode(".jpg", imgFace)
+if err != nil {
+    log.Printf("unable to encode matrix: %v", err)
+    continue
+}
+
+faces, err := fbox.Check(bytes.NewReader(buf))
+
+
+if err != nil {
+    log.Println("unable to recognize face: %v", err)
+}
+
+log.Println("have faces %v",faces)
+
+var caption = "I don't know you"
+if len(faces) > 0 {
+    caption = fmt.Sprintf("I know you %s", faces[0].Name)
+}
 
 			 // draw rectangle for the face
 			 size := gocv.GetTextSize("I don't know you", gocv.FontHersheyPlain, 3, 2)
 			 pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
-			 gocv.PutText(&img, "I don't know you", pt, gocv.FontHersheyPlain, 3, blue, 2)
+			 gocv.PutText(&img, caption, pt, gocv.FontHersheyPlain, 3, blue, 2)
 			 gocv.Rectangle(&img, r, blue, 3)
 
 			 // show the image in the window, and wait 100ms
